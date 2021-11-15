fa31b055f396b8e0fa3653139519bc5b391044a5
Marcos Antonio dos Santos <marcosantonio.dev@gmail.com>
2021-11-15 09:52:31 -0300

    add listener to putAdress event

--- app/assets/src/App.js
+++ app/assets/src/App.js
@@ -5,6 +5,23 @@ client.on("app.registered", (e) => {
   client.invoke("resize", { width: "100%", height: "130px" });
 });
 
+const dados = await client.get(['ticket.id'])
+
+
+client.on('putAdress', (content) => {
+  client.request({
+    url: `/api/v2/tickets/${dados['ticket.id']}.json`, 
+    type: 'PUT',
+    data: {
+      ticket: {
+        comment: {
+          body: content
+        }
+      }
+    }
+  })
+})
+
 // Create screen context
 import Main from "./Main.js";
 Main();
