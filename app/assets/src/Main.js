de73ddf464690faca426e39311fcda05a4a60146
Marcos Antonio dos Santos <marcosantonio.dev@gmail.com>
2021-11-15 09:52:07 -0300

    add form and submit function

--- app/assets/src/Main.js
+++ app/assets/src/Main.js
@@ -9,10 +9,21 @@ client.metadata().then((metadata) => {
 
 const Main = async () => {
   const App = document.getElementById("app");
-  let appBody = `<div id="main-content"></div>`;
+  const F_buscarCep = async (evt) => {
+    evt.preventDefault()
+
+    const cep = document.getElementById("_cep").value
+    const res = await fetch(`https://viacep.com.br/ws/${cep}/json/`, { method: 'GET' })
+    const json = await res.json()
+
+    client.trigger('putAdress', `Você fala de ${json.localidade}?`)
+  }
+  let appBody = `<div id="main-content"><form id="form"><input type="text" name="_cep" id="_cep" /><br/><button type="submit">Buscar</button></form></div>`;
 
   // Write App
   App.innerHTML = appBody;
+
+  document.getElementById("form").addEventListener('submit', F_buscarCep)
 };
 
 export default Main;
